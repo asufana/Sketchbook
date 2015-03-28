774421fe76c3757b6ea0868fe4a062e32877545b
hana <asufanah@gmail.com>
2015-03-28 20:27:58 +0900

    Added APT sample ( in.tombo.ponto ).

---
+++ src/main/java/com/github/asufana/ponta/PontaConfig.java
@@ -0,0 +1,8 @@
+package com.github.asufana.ponta;
+
+import in.tombo.ponto.*;
+
+@ConstantResource
+public class PontaConfig {
+    
+}
---
+++ src/test/java/com/github/asufana/ponta/PontaConfigTest.java
@@ -0,0 +1,9 @@
+package com.github.asufana.ponta;
+
+import org.junit.*;
+
+public class PontaConfigTest {
+    
+    @Test
+    public void test() {}
+}
